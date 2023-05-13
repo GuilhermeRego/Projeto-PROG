7ffd90e93e0c59b1b8f464683dffb76742c846b4
GuilhermeRego <132831104+GuilhermeRego@users.noreply.github.com>
2023-05-13 16:09:09 +0100

    Update Image.cpp
    Comentários

--- Image.cpp
+++ Image.cpp
@@ -2,31 +2,34 @@
 
 namespace prog
 {
-  //Cria uma imagem com largura w, altura h e todos os pixeis
-  //implementados com a cor fill, definido em Image.hpp
+  // Creates an image with width w and height h and every pixel
+  // is set with the color of fill, defined in Image.hpp
   Image::Image(int w, int h, const Color &fill) : width_(w), height_(h), pixels_(h, std::vector<Color>(w, fill))
-  {
+  { // Implemented in Image.hpp
   }
   Image::~Image()
-  {
+  { // No need for a destructor
   }
 
-  //Obter valores de largura e altura
+  // Get width function 
   int Image::width() const
   {
     return width_;
   }
+  
+  // Get height function
   int Image::height() const
   {
     return height_;
   }
 
-  //Obter cores da imagem num pixel específico mutável
+  // Get the color of a mutable pixel in the image
   Color& Image::at(int x, int y)
   {
     return pixels_[y][x];
   }
-  //Obter cores da imagem num pixel
+  
+  // Get the color of a pixel in the image
   const Color& Image::at(int x, int y) const
   {
     return pixels_[y][x];
